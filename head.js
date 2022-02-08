f594ff203c5b2c1d304edb81e40dda3cd8a259ba
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-08 10:07:22 -0700

    created head function

---
+++ head.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head = function(arr) {
+  return arr[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+console.log(assertEqual(head([5, 6, 7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"])), "Hello");
