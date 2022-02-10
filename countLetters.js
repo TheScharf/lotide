298a0edb32b57f075efecd90540cdded8efcb34f
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-09 20:33:36 -0700

    created a function for counting letters within a string

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(characters) {
+  let lettersCounted = {};
+  for (const individual of characters) {
+    if (lettersCounted.hasOwnProperty(individual)) {
+      lettersCounted[individual] = lettersCounted[individual] + 1;
+    } else {
+      lettersCounted[individual] = 1;
+    }
+    if (characters === " ") {
+      delete lettersCounted[individual];
+    }
+  }
+  return lettersCounted;
+};
+
+console.log(countLetters("Lighthouse in the house"));
