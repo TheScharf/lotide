0d0656969c9d7e96846ca228e553421c894945e7
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-14 15:06:34 -0700

    refactored middle function

--- middle.js
+++ middle.js
@@ -1,27 +1,3 @@
-/*const eqArrays = function(arr1, arr2) {
-  if (!(Array.isArray(arr1) || !(Array.isArray(arr2)))) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-    if (arr1.length !== arr2.length) {
-      return false;
-    }
-  }
-  return true;
-};*/
-
-/*const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};*/
-
-//Find and return the middle od an array index
 
 const middle = function(array) {
   let midArr = [];
@@ -41,7 +17,4 @@ const middle = function(array) {
 };
 
 
-
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3, 4, 5]));
+module.exports = middle;
