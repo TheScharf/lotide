697218ff1873e9aa74bfc580c829281ed07f5eea
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-13 21:17:58 -0700

    created function to find the key of an object when given a value within that key

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, callback) {
+  let keyedObj = Object.keys(object);
+  let item = 0;
+  for (let key in object) {
+    if (object[key] === callback) {
+      return keyedObj[item];
+    }
+    item++;
+  }
+return undefined;
+}
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
