c33fad95ad5f7ae91e31ec991c1173f8fda5b673
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-14 16:17:07 -0700

    fixed error in tail.js

--- tail.js
+++ tail.js
@@ -2,7 +2,10 @@
 const assertEqual = require('./assertEqual');
 
 const tail = function(arr) {
-  let newArr = arr.toString().slice(1);
+  let newArr = [];
+  for (let i = 1; i <= arr.length - 1; i++) {
+    newArr.push(arr[i]);
+  }
   return newArr;
 };
 
