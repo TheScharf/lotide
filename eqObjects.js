0913f915ad2b5f7d3da5a7193b04063df3b3d562
Alexandra Scharfenberger <ascharfe55@gmail.com>
2022-02-14 09:09:42 -0700

    corrected an error

--- eqObjects.js
+++ eqObjects.js
@@ -6,7 +6,7 @@ const eqObjects = function(object1, object2) {
     return false;
   }
   for (let key in object1) {
-    if (object1[key] !== (object2[key])) {
+    if ((object1[key]) !== (object2[key])) {
       return false;
     }
   }
